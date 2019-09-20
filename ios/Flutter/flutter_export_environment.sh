582a5e8fcabfe5064e3174613ae25dcd6e8bfc91
Michael Bentley <mbentley124@gmail.com>
2019-09-20 00:25:53 -0400

    Ios now builds (crashes on startup though)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Michael/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/Michael/Documents/LUCheeseApp"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/Michael/Documents/LUCheeseApp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/Michael/Documents/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.1.0"
 export "FLUTTER_BUILD_NUMBER=1"
